2f7d102ba4201b862b6c118a359a25e64a5ea059
laurelisthompson <laurelis.thompson@gmail.com>
2020-11-17 14:23:54 -0500

    create user auth setup

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,24 +1,26 @@
 class Api::SessionsController < ApplicationController
 
-    before_action :ensure_logged_in, only: [:destroy]
-
     def create
         @user = User.find_by_credentials(
             params[:user][:username],
-            params[:user][:email],
             params[:user][:password]
         )
         if @user
             login!(@user)
             render "api/users/show"
         else
-            render json: ["Invalid username and password combination"], status: 422
+            render json: ["Invalid username and password combination"], status: 401
         end
     end
 
     def destroy
-        logout!
+    @user = current_user
+    if @user
+        logout
         render "api/users/show"
+    else
+        render json: ["Nobody is signed in"], status: 404
     end
+  end
 
 end
--- frontend/giva.jsx
+++ frontend/giva.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//
+// import { login, logout, signup } from "./util/session_api_util"
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Giva!</h1>, root);
+    ReactDOM.render(<h1>Giva</h1>, root);
+
+    // TESTING
+    // window.login = login;
+    // window.logout = logout;
+    // window.signup = signup;
 })
