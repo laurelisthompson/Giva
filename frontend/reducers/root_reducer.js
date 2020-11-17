b8c369e4690f6a734c29853d1f448d5b43f2b037
laurelisthompson <laurelis.thompson@gmail.com>
2020-11-17 14:39:04 -0500

    create reducers and combine

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+import entitiesReducer from './entities_reducer';
+import errorsReducer from './errors_reducer';
+import sessionErrorsReducer from './session_errors_reducer';
+import sessionReducer from './session_reducer';
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    errors: errorsReducer,
+    session: sessionReducer
+});
+
+export default rootReducer;
