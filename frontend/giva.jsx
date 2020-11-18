bca2409f93a686a1c9ff5a56d090fc29b1ed0ef9
laurelisthompson <laurelis.thompson@gmail.com>
2020-11-18 10:29:52 -0500

    fix login/signup functionality

--- frontend/giva.jsx
+++ frontend/giva.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
 
-// import { login, logout, signup } from "./util/session_api_util"
+import { login, logout, signup } from "./actions/session_actions"
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(<Root store={store}/>, root);
 
     // TESTING
-    // window.login = login;
-    // window.logout = logout;
-    // window.signup = signup;
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
+    window.login = login;
+    window.logout = logout;
+    window.signup = signup;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
