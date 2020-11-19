be4c609dc50e5e2391fe1970e4666ece2d3e0d5b
laurelisthompson <laurelis.thompson@gmail.com>
2020-11-18 21:13:51 -0500

    add css styling

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -47,7 +47,7 @@ class SessionForm extends React.Component {
     render() {
         if (this.props.formType === 'sign in') {
             return (
-                <div>
+                <div class="content-background">
                     <nav class="login-nav">
                         <h1>Giva</h1>
                     </nav>
@@ -56,8 +56,8 @@ class SessionForm extends React.Component {
                             <p class="login-title">{'Sign in below'}</p>
                             <p class="login-title">or {this.props.navLink}</p>
                             {this.renderErrors()}
-                            <p>{'Sign in the same way you did last time'}</p>
-                            <p>{'to avoid creating a second Kiva account.'}</p>
+                            <p>{'Sign in the same way you did last time'}<br/>
+                            {'to avoid creating a second Kiva account.'}</p>
                             <div>
                                 <label>Email
                                 <br />
@@ -90,52 +90,59 @@ class SessionForm extends React.Component {
             )
         } else {
             return (
-                <div class="signup-form">
-                    <form onSubmit={this.handleSubmit}>
-                        <nav class="signup-nav">
-                            <h1>Giva</h1>
-                        </nav>
-                        <p>{'Create a new account below'}</p>
-                        <p>or {this.props.navLink}</p>
-                        {this.renderErrors()}
-                        <div>
-                            <label>First name
-                            <br />
-                                <input type="text"
-                                    value={this.state.first_name}
-                                    onChange={this.update('first_name')}
+                <div>
+                    <nav class="signup-nav">
+                        <h1>Giva</h1>
+                    </nav>
+                    <div class="signup-form">
+                        <form onSubmit={this.handleSubmit} class="signup-form-box">
+                            <p class="signup-title">{'Create a new account below'}</p>
+                            <p class="signup-title">or {this.props.navLink}</p>
+                            {this.renderErrors()}
+                            <div>
+                                <label>First name
+                                <br />
+                                    <input type="text"
+                                        value={this.state.first_name}
+                                        onChange={this.update('first_name')}
+                                        class="signup-input-box"
+                                    />
+                                </label>
+                                <br/>
+                                <label>Last name
+                                <br />
+                                    <input type="text"
+                                        value={this.state.last_name}
+                                        onChange={this.update('last_name')}
+                                        class="signup-input-box"
+                                    />
+                                </label>
+                                <br />
+                                <label>Email
+                                <br />
+                                    <input type="text"
+                                        value={this.state.email}
+                                        onChange={this.update('email')}
+                                        class="signup-input-box"
+                                    />
+                                </label>
+                                <br />
+                                <label>Create password
+                                <br />
+                                    <input type="password"
+                                        value={this.state.password}
+                                        onChange={this.update('password')}
+                                        class="signup-input-box"
+                                    />
+                                </label>
+                                <br />
+                                <input type="submit"
+                                    value={'Create new account'}
+                                    class="submit-button"
                                 />
-                            </label>
-                            <br/>
-                            <label>Last name
-                            <br />
-                                <input type="text"
-                                    value={this.state.last_name}
-                                    onChange={this.update('last_name')}
-                                />
-                            </label>
-                            <br />
-                            <label>Email
-                            <br />
-                                <input type="text"
-                                    value={this.state.email}
-                                    onChange={this.update('email')}
-                                />
-                            </label>
-                            <br />
-                            <label>Create password
-                            <br />
-                                <input type="password"
-                                    value={this.state.password}
-                                    onChange={this.update('password')}
-                                />
-                            </label>
-                            <br />
-                            <input type="submit"
-                                value={'Create new account'}
-                            />
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             )
         }
